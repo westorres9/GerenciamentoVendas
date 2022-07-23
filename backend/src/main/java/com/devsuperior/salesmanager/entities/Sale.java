f2cd35a42d312f0e5af492e855d61a83f5df3d50
westorres9 <wester.torres9@gmail.com>
2022-07-23 08:53:52 -0300

    projeto criado

---
+++ backend/src/main/java/com/devsuperior/salesmanager/entities/Sale.java
@@ -0,0 +1,67 @@
+package com.devsuperior.salesmanager.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Sale {
+
+    private Long id;
+    private LocalDate date;
+    private Integer visited;
+    private Double amount;
+
+    public Sale() {
+    }
+
+    public Sale(Long id, LocalDate date, Integer visited, Double amount) {
+        this.id = id;
+        this.date = date;
+        this.visited = visited;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Integer visited) {
+        this.visited = visited;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sale sale = (Sale) o;
+        return Objects.equals(id, sale.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
