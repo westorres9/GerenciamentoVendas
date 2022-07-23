38f5647999ac94427c54d7d164848ab9380b85b4
westorres9 <wester.torres9@gmail.com>
2022-07-23 11:15:35 -0300

    SaleService and SaleResource FindById, Insert, Update, Delete Operations

--- backend/src/main/java/com/devsuperior/salesmanager/resources/SaleResource.java
+++ backend/src/main/java/com/devsuperior/salesmanager/resources/SaleResource.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -21,5 +22,34 @@ public class SaleResource {
     public ResponseEntity<Page<SaleDTO>> findAllPaged(Pageable pageable) {
         Page<SaleDTO> page = service.findAllPaged(pageable);
         return ResponseEntity.ok().body(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SaleDTO> findById(@PathVariable  Long id) {
+        SaleDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<SaleDTO> insert(@RequestBody SaleDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<SaleDTO> update(@PathVariable Long id, @RequestBody SaleDTO dto) {
+        SaleDTO newDto = service.update(id, dto);
+        return ResponseEntity.ok().body(newDto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- backend/src/main/java/com/devsuperior/salesmanager/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/salesmanager/services/SaleService.java
@@ -3,11 +3,20 @@ package com.devsuperior.salesmanager.services;
 import com.devsuperior.salesmanager.DTO.SaleDTO;
 import com.devsuperior.salesmanager.entities.Sale;
 import com.devsuperior.salesmanager.repositories.SaleRepository;
+import com.devsuperior.salesmanager.repositories.UserRepository;
+import com.devsuperior.salesmanager.services.exceptions.DatabaseException;
+import com.devsuperior.salesmanager.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.lang.module.ResolutionException;
+import java.util.Optional;
 
 @Service
 public class SaleService {
@@ -15,9 +24,62 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Transactional(readOnly = true)
     public Page<SaleDTO> findAllPaged(Pageable pageable) {
         Page<Sale> page = repository.findAll(pageable);
         return page.map(x -> new SaleDTO(x));
     }
+
+    @Transactional(readOnly = true)
+    public SaleDTO findById(Long id) {
+        Optional<Sale> obj = repository.findById(id);
+        Sale entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
+        return new SaleDTO(entity);
+    }
+
+    @Transactional
+    public SaleDTO insert(SaleDTO dto) {
+        Sale entity = new Sale();
+        entity.setDate(dto.getDate());
+        entity.setVisited(dto.getVisited());
+        entity.setDeals(dto.getDeals());
+        entity.setAmount(dto.getAmount());
+        entity.setSeller(userRepository.getReferenceById(dto.getSellerId()));
+        entity = repository.save(entity);
+        return new SaleDTO(entity);
+    }
+
+    @Transactional
+    public SaleDTO update(Long id, SaleDTO dto) {
+        try {
+            Sale entity = repository.getReferenceById(id);
+            entity.setDate(dto.getDate());
+            entity.setVisited(dto.getVisited());
+            entity.setDeals(dto.getDeals());
+            entity.setAmount(dto.getAmount());
+            entity.setSeller(userRepository.getReferenceById(dto.getSellerId()));
+            entity = repository.save(entity);
+            return new SaleDTO(entity);
+        }
+        catch (EntityNotFoundException e ) {
+            throw new ResourceNotFoundException("Entity not found");
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found" + id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+
+
 }
