ff91fcbc3d5cb65aed5adfdf60d19e67b0df11bc
westorres9 <wester.torres9@gmail.com>
2022-07-23 14:06:04 -0300

    UserInsertValid

--- backend/src/main/java/com/devsuperior/salesmanager/services/validations/UserInsertValidator.java
+++ backend/src/main/java/com/devsuperior/salesmanager/services/validations/UserInsertValidator.java
@@ -1,8 +1,15 @@
 package com.devsuperior.salesmanager.services.validations;
 
 import com.devsuperior.salesmanager.DTO.UserInsertDTO;
+import com.devsuperior.salesmanager.entities.User;
 import com.devsuperior.salesmanager.repositories.UserRepository;
+import com.devsuperior.salesmanager.resources.exceptions.FieldMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
 
