ffc85cbd92ed88fa77e37bd52e5af3202323410f
westorres9 <wester.torres9@gmail.com>
2022-08-24 13:24:53 -0300

    correção se token for nulo ou undefined

--- frontend/js/TokenGlobalInterceptor.js
+++ frontend/js/TokenGlobalInterceptor.js
@@ -4,10 +4,16 @@ vendasApp.factory('tokenGlobalInterceptor', Interceptor);
     return {
         request: function(config) {
             const token = AuthService().getToken();
+            if(token === null ){
+                console.log(token);
+                console.log(config);
+                return config;
+            }
+            else{
                 config.headers['Authorization'] = 'Bearer ' + token.access_token;
                 console.log(config);
                 return config;
-            },
+            }},
             responseError: function(error) {
                 if (error.status === 401 || error.status === 403) {
                     console.log(config);
